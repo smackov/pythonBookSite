e375a9c2821e422575740885e2bcbd53b4c9c3a4
smackov <smackovdinislam@gmail.com>
2021-01-14 10:43:23 +0500

    Registrate models in 'admin panel'
    Registrated models:
     - Article
     - Section
     - Chapter

--- pythonBookSite/book/admin.py
+++ pythonBookSite/book/admin.py
@@ -3,6 +3,29 @@ from django.contrib import admin
 from .models import Chapter, Section, Article
 
 
-admin.site.register(Chapter)
-admin.site.register(Section)
-admin.site.register(Article)
+class SectionInline(admin.TabularInline):
+    model = Section
+    
+    
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'serial_number')
+    inlines = [SectionInline]
+
+
+class ArticleInline(admin.TabularInline):
+    model = Article
+    
+    
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'serial_number', 'parent_chapter')
+    inlines = [ArticleInline]
+    list_filter = ('parent_chapter',)    
+    
+    
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'serial_number', 'parent_section')
+    list_filter = ('parent_section',)
+    
