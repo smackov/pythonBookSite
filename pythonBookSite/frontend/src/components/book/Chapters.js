4ca3dda2aad4f9ad9f639a57ee2fecb7b28ccf09
smackov <smackovdinislam@gmail.com>
2021-01-16 23:12:06 +0500

    Add Content sidebar at index page

--- pythonBookSite/frontend/src/components/book/Chapters.js
+++ pythonBookSite/frontend/src/components/book/Chapters.js
@@ -16,34 +16,50 @@ export class Chapters extends Component {
     render() {
         return (
             <Fragment>
-                <div className="chapter-list">
-                {this.props.chapters.map(chapter => {
-                    return (
-                        <div className="chapter-item border-bottom mb-4">
-                            <span class="badge badge-primary">Часть {chapter.serial_number}</span>
-                            <h1 className="text-20rem my-2">{chapter.name}</h1>
-                            <div className="section-list my-4 px-2">
-                                {chapter.child_sections.map(section => {
-                                    return (
-                                        <div className="section-item my-2">
-                                            <h2 className="text-15rem">{section.name}</h2>
-                                            <ul className="article-list mx-0 px-0">
-                                                {section.child_articles.map(article => {
-                                                    return (
-                                                        <li className="article-item list-unstyled">
-                                                            {article.serial_number} &nbsp;
-                                                            <a href="#">{article.name}</a>
-                                                        </li>
-                                                    )
-                                                })}
-                                            </ul>
+                <div className="row my-4">
+                    <div className="col-lg-10">
+                        <div className="chapter-list">
+                            {this.props.chapters.map(chapter => {
+                                return (
+                                    <div className="chapter-item border-bottom mb-4">
+                                        <span class="badge badge-primary">Часть {chapter.serial_number}</span>
+                                        <h1 className="text-20rem my-2" id={chapter.id}>{chapter.name}</h1>
+                                        <div className="section-list my-4 px-2">
+                                            {chapter.child_sections.map(section => {
+                                                return (
+                                                    <div className="section-item my-2">
+                                                        <h2 className="text-15rem">{section.name}</h2>
+                                                        <ul className="article-list mx-0 px-0">
+                                                            {section.child_articles.map(article => {
+                                                                return (
+                                                                    <li className="article-item list-unstyled">
+                                                                        {article.serial_number} &nbsp;
+                                                                        <a href="#">{article.name}</a>
+                                                                    </li>
+                                                                )
+                                                            })}
+                                                        </ul>
+                                                    </div>
+                                                )
+                                            })}
                                         </div>
-                                    )
-                                })}
-                            </div>
+                                    </div>
+                                );
+                            })}
                         </div>
-                    );
-                })}
+                    </div>
+                    <div className="col-lg-2 border-left">
+                        <h3 className="text-15rem">Content:</h3>
+                        <ul className="content-list mx-0 px-0">
+                            {this.props.chapters.map(chapter => {
+                                return (
+                                    <li className="content-item list-unstyled">
+                                        <a href={'#' + chapter.id}>{chapter.name}</a>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
                 </div>
             </Fragment>
         )
--- pythonBookSite/frontend/src/components/book/Dashboard.js
+++ pythonBookSite/frontend/src/components/book/Dashboard.js
@@ -4,13 +4,6 @@ import Chapters from './Chapters'
 
 export default function Dashboard() {
     return (
-        <div className="row my-4">
-            <div className="col-lg-10">
-                <Chapters />
-            </div>
-            <div className="col-lg-2 bg-light">
-                <h2>List</h2>
-            </div>
-        </div>
+        <Chapters />
     )
 }
