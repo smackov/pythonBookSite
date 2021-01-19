56f691d92c0c00b643f5684e703667d8da06b123
smackov <smackovdinislam@gmail.com>
2021-01-20 00:31:07 +0500

    Fix: wrong margins in foote, header and article elements

--- pythonBookSite/frontend/src/components/layout/Footer.js
+++ pythonBookSite/frontend/src/components/layout/Footer.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export class Footer extends Component {
     render() {
         return (
-            <footer className="footer mt-auto bg-dark p-1 pt-2 mb-0">
-                <div className="container text-center">
-                    <p className="text-decoration-none">
+            <footer className="footer mt-auto bg-dark p-2 mb-0">
+                <div className="container text-center my-0">
+                    <p className="text-decoration-none my-0">
                         This web application was created by
                         <a className="text-reset pl-2" href="https://github.com/smackov">
                             <span className="badge badge-light">Smackov</span>
