c8aff9684c4298a7aa50f9891dbd39138c9d3235
smackov <smackovdinislam@gmail.com>
2021-02-13 23:49:17 +0500

    Fix: forgot add error reducer in index reducers

--- pythonBookSite/frontend/src/reducers/index.js
+++ pythonBookSite/frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import chapters from './chapters';
 import article from './article';
+import errors from './errors';
 
 export default combineReducers({
     chapters,
-    article
+    article,
+    errors
 });
--- pythonBookSite/frontend/static/frontend/main.js
+++ pythonBookSite/frontend/static/frontend/main.js
@@ -5298,6 +5298,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _act
 
 /***/ }),
 
+/***/ "./pythonBookSite/frontend/src/reducers/errors.js":
+/*!********************************************************!*\
+  !*** ./pythonBookSite/frontend/src/reducers/errors.js ***!
+  \********************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _actions_types__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../actions/types */ \"./pythonBookSite/frontend/src/actions/types.js\");\n\nvar initialState = {\n  msg: {},\n  status: null\n};\n/* harmony default export */ __webpack_exports__[\"default\"] = (function () {\n  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : initialState;\n  var action = arguments.length > 1 ? arguments[1] : undefined;\n\n  switch (action.type) {\n    case _actions_types__WEBPACK_IMPORTED_MODULE_0__[\"GET_ERRORS\"]:\n      return {\n        msg: action.payload.msg,\n        status: action.payload.status\n      };\n\n    default:\n      return state;\n  }\n});\n\n//# sourceURL=webpack:///./pythonBookSite/frontend/src/reducers/errors.js?");
+
+/***/ }),
+
 /***/ "./pythonBookSite/frontend/src/reducers/index.js":
 /*!*******************************************************!*\
   !*** ./pythonBookSite/frontend/src/reducers/index.js ***!
@@ -5306,7 +5318,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _act
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var redux__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! redux */ \"./node_modules/redux/es/redux.js\");\n/* harmony import */ var _chapters__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./chapters */ \"./pythonBookSite/frontend/src/reducers/chapters.js\");\n/* harmony import */ var _article__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./article */ \"./pythonBookSite/frontend/src/reducers/article.js\");\n\n\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Object(redux__WEBPACK_IMPORTED_MODULE_0__[\"combineReducers\"])({\n  chapters: _chapters__WEBPACK_IMPORTED_MODULE_1__[\"default\"],\n  article: _article__WEBPACK_IMPORTED_MODULE_2__[\"default\"]\n}));\n\n//# sourceURL=webpack:///./pythonBookSite/frontend/src/reducers/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var redux__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! redux */ \"./node_modules/redux/es/redux.js\");\n/* harmony import */ var _chapters__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./chapters */ \"./pythonBookSite/frontend/src/reducers/chapters.js\");\n/* harmony import */ var _article__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./article */ \"./pythonBookSite/frontend/src/reducers/article.js\");\n/* harmony import */ var _errors__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./errors */ \"./pythonBookSite/frontend/src/reducers/errors.js\");\n\n\n\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Object(redux__WEBPACK_IMPORTED_MODULE_0__[\"combineReducers\"])({\n  chapters: _chapters__WEBPACK_IMPORTED_MODULE_1__[\"default\"],\n  article: _article__WEBPACK_IMPORTED_MODULE_2__[\"default\"],\n  errors: _errors__WEBPACK_IMPORTED_MODULE_3__[\"default\"]\n}));\n\n//# sourceURL=webpack:///./pythonBookSite/frontend/src/reducers/index.js?");
 
 /***/ }),
 
