654955c19fb000bf9461de3ef7aff3a9539e223e
smackov <smackovdinislam@gmail.com>
2021-02-17 22:59:01 +0500

    Add 'register' api

--- pythonBookSite/accounts/serializers.py
+++ pythonBookSite/accounts/serializers.py
@@ -19,4 +19,18 @@ class LoginSerializer(serializers.Serializer):
         if user and user.is_active:
             return user
         raise serializers.ValidationError("Incorrect Credentials")
-  
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+        
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data['username'], 
+            validated_data['email'], 
+            validated_data['password'],
+        )
+        return user  
+    
--- pythonBookSite/accounts/urls.py
+++ pythonBookSite/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from knox import views as knox_views
 
-from .api import LoginAPI, UserAPI
+from .api import LoginAPI, UserAPI, RegisterAPI
 
 
 urlpatterns = [
     path('api/auth', include('knox.urls')),
     path('api/auth/login', LoginAPI.as_view()),
     path('api/auth/user', UserAPI.as_view()),
+    path('api/auth/register', RegisterAPI.as_view()),
 ]
