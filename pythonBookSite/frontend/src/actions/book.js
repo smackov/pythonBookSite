cad5efb7e7f73e6cee578f33bd82e3f9dd2f5a49
smackov <smackovdinislam@gmail.com>
2021-02-17 23:18:31 +0500

    Add 'token configs' to 'book' actions

--- pythonBookSite/frontend/src/actions/book.js
+++ pythonBookSite/frontend/src/actions/book.js
@@ -2,11 +2,12 @@ import axios from 'axios';
 
 import { GET_CHAPTERS, GET_ARTICLE, GET_ERRORS } from './types';
 import { returnErrors } from './messages';
+import { tokenConfig } from './auth';
 
 // GET CHAPTERS
-export const getChapters = () => dispatch => {
+export const getChapters = () => (dispatch, getState) => {
     axios
-        .get('/api/chapters/')
+        .get('/api/chapters/', tokenConfig(getState))
         .then(res => {
             dispatch({
                 type: GET_CHAPTERS,
@@ -19,9 +20,9 @@ export const getChapters = () => dispatch => {
 };
 
 // GET ARTICLE
-export const getArticle = (id) => dispatch => {
+export const getArticle = (id) => (dispatch, getState) => {
     axios
-        .get(`/api/article/${id}/`)
+        .get(`/api/article/${id}/`, tokenConfig(getState))
         .then(res => {
             dispatch({
                 type: GET_ARTICLE,
